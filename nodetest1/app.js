1e2455802d71989fda15e8ee95080d8a8f72b953
Tom kowalski <kowalski.tom0@gmail.com>
2015-04-12 01:20:59 -0400

    online migaration

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -7,14 +7,14 @@ var bodyParser = require('body-parser');
 
 // New Code
 
-var mongoUri = process.env.MONGOLAB_URI ||
-    'mongodb://localhost/data';
+var mongoUri = 'mongodb://heroku_app35793701:4jsstjv7n4b2bde0oua11vb3ab@ds061611.mongolab.com:61611/heroku_app35793701?replicaSet=rs-ds061611'; //||
+    //'mongodb://localhost/data';
 
 var mongo = require('mongodb');
 var monk = require('monk')
 	, db = monk(mongoUri)
 	, collection = db.get('collection');
-var db = monk('localhost:27017/data');
+var db = monk(mongoUri);   //'localhost:27017/data');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -23,6 +23,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', (process.env.PORT || 5000));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
