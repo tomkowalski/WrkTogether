dd8def75d63e7eb8a831d06711f99419b68807b5
Tom kowalski <kowalski.tom0@gmail.com>
2015-04-12 13:42:12 -0400

    final for hackathon

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -4,10 +4,12 @@ var util = require('util');
 
 
 /* GET home page. */
+router.get('/Home', function(req, res, next) {
+  res.render('Home', { title: 'Express' });
+});
 router.get('/', function(req, res, next) {
   res.render('Home', { title: 'Express' });
 });
-
 /* GET Hello World page. */
 router.get('/helloworld', function(req, res) {
     res.render('helloworld', { title: 'Hello, World!' })
@@ -16,7 +18,7 @@ router.get('/helloworld', function(req, res) {
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
     var db = req.db;
-    var collection = db.get('usercollection');
+    var collection = db.get('postcollection');
     collection.find({},{},function(e,docs){
         res.render('userlist', {
             "userlist" : docs
@@ -44,12 +46,15 @@ router.get('/LowerBack', function(req, res) {
     res.render('LowerBack', {title: 'Login'});
 });
 
-router.get('/UperBack', function(req, res) {
+router.get('/UpperBack', function(req, res) {
     res.render('UpperBack', {title: 'Login'});
 });
 
 router.get('/Biceps', function(req, res) {
     res.render('Biceps', {title: 'Login'});
+})
+router.get('/Lats', function(req, res) {
+    res.render('Lats', {title: 'Login'});
 });
 
 router.get('/Triceps', function(req, res) {
@@ -75,47 +80,89 @@ router.get('/Calves', function(req, res) {
     res.render('Calves', {title: 'Login'});
 });
 
+router.get('/Post', function(req, res) {
+    res.render('Post', {title: 'Login'});
+});
+
 router.get('/Login', function(req, res) {
     res.render('Login', {title: 'Login'});
 });
-
-/* GET New User page. */
-router.get('/newuser', function(req, res) {
-    res.render('newuser', { title: 'Add New User' });
+router.get('/Register', function(req, res) {
+    res.render('Register', {title: 'Login'});
 });
-
 /* POST to Add User Service */
-router.post('/Login', function(req, res) {
+router.post('/postContent', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
 
     // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-    var userPassword = req.body.password;
-
-
-    // Set our collection
-    var collection = db.get('usercollection');
-
+    var title = req.body.titlePost;
+    var text = req.body.textPost;
+    var body = req.body.bodyPart;
+    
+    var collection = db.get('postcollection');
     // Submit to the DB
     collection.insert({
-        "username" : userName,
-        "email" : userEmail,
-        "password" : userPassword,
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            res.location("userlist");
-            // And forward to success page
-            res.redirect("userlist");
-        }
-    });
+        "title" : title,
+        "text" : text,
+        "body" : body
+
+        }, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                // If it worked, set the header so the address bar doesn't still say /adduser
+                res.location("Home");
+                // And forward to success page
+                res.redirect("Home");
+            }
+        });
+});
+
+/* POST to Add User Service */
+router.post('/addUser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userFirst = req.body.firstName;
+    var userLast = req.body.lastName;
+    var userEmail = req.body.email;
+    var confEmail = req.body.confEmail;
+    var userPassword = req.body.password;
+    var confPassword = req.body.confPass;
+    var collection = db.get('usercollection');
+    if(confEmail === userEmail && userPassword === confPassword) {
+       
+       // Submit to the DB
+        collection.insert({
+            "firstName" : userFirst,
+            "lastName" : userLast,
+            "email" : userEmail,
+            "password" : userPassword
+
+        }, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                // If it worked, set the header so the address bar doesn't still say /adduser
+                res.location("Home");
+                // And forward to success page
+                res.redirect("Home");
+            }
+        });
+    }
+    else {
+        res.location("Register");
+                // And forward to success page
+        res.redirect("Register");
+    }
 });
 
 
